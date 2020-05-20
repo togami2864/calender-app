e69855e43ea566531b61ce9b77193437b8cd8df2
togami <tuabobo123@gmail.com>
2020-05-20 16:56:34 +0900

    step3-4 logic

--- front/src/services/calendar.js
+++ front/src/services/calendar.js
@@ -13,6 +13,20 @@ export const createCalendar = month => {
         });
 };
 
+const getMonthStateCreator = diff => month => {
+    const day = getMonth(month).add(diff, "month");
+    return formatMonth(day);
+};
+
+export const getNextMonth = getMonthStateCreator(1);
+export const getPreviousMonth = getMonthStateCreator(-1);
+
+
+export const formatMonth = day => ({
+    month: day.month() + 1,
+    year: day.year()
+});
+
 export const getMonth = ({ year, month }) => {
     return dayjs(`${year}-${month}`);
 };
