6e0f4c121ea206d5b0273d1df50f615d81fb0d70
togami <tuabobo123@gmail.com>
2020-05-20 16:19:44 +0900

    step2-4

--- front/src/components/CalendarBoard/index.jsx
+++ front/src/components/CalendarBoard/index.jsx
@@ -3,24 +3,9 @@ import React from "react";
 import { GridList } from "@material-ui/core";
 import * as styles from "./style.css";
 
-import dayjs from "dayjs";
-import "dayjs/locale/ja";
+import CalendarElement from "../CalendarElement";
+import { createCalendar } from "../../services/calendar";
 
-dayjs.locale("ja");
-
-const createCalendar = () => {
-    const firstDay = dayjs().startOf("month");// 今月の最初の日のデータ
-    const firstDayIndex = firstDay.day();//最初の日の曜日
-    console.log(firstDayIndex)
-    return Array(35)
-        .fill(0)
-        .map((_, i) => {
-            const diffFromFirstDay = i - firstDayIndex;
-            const day = firstDay.add(diffFromFirstDay, "day");
-
-            return day;
-        });
-};
 const calendar = createCalendar();
 
 
@@ -32,7 +17,7 @@ const CalendarBoard = () => {
             <GridList className={styles.grid} cols={7} spacing={0} cellHeight="auto">
                 {calendar.map(c => (
                     <li key={c.toISOString()}>
-                        <div className={styles.element}>{c.format("D")}</div>
+                        <CalendarElement>{c.format("D")}</CalendarElement>
                     </li>
                 ))}
             </GridList>
--- front/src/index.jsx
+++ front/src/index.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import dayjs from "dayjs";
+import "dayjs/locale/ja";
+
+dayjs.locale("ja");
+
 import CalendarBoard from "./components/CalendarBoard/index";
 
 const App = () => (
