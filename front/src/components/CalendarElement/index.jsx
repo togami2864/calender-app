1cbe92003435f5eef2f78b5ee5a82a2fd91d009c
togami <tuabobo123@gmail.com>
2020-05-20 16:34:09 +0900

    step2-6

--- front/src/components/CalendarElement/index.jsx
+++ front/src/components/CalendarElement/index.jsx
@@ -1,21 +1,36 @@
 import React from "react";
 
+import dayjs from "dayjs";
+
 import { Typography } from "@material-ui/core";
 
 import * as styles from "./style.css";
+import { isSameMonth, isFirstDay, isSameDay } from "../../services/calendar";
 
 const CalendarElement = ({ day }) => {
-    const isFirstDay = day.date() === 1;
-    const format = isFirstDay ? "M月D日" : "D";
+
+    const today = dayjs();
+    // 当日かどうか判断
+    const isToday = isSameDay(day, today);
+    //初日かどうか
+    const format = isFirstDay(day) ? "M月D日" : "D";
+
+    // 今月以外をグレーダウン
+    const isCurrentMonth = isSameMonth(day, today);
+    const textColor = isCurrentMonth ? "textPrimary" : "textSecondary";
+
     return (
         <div className={styles.element}>
             <Typography
                 className={styles.date}
                 align="center"
+                color={textColor}
                 variant="caption"
                 component="div"
             >
-                {day.format(format)}
+                <span className={isToday ? styles.today : ""}>
+                    {day.format(format)}
+                </span>
             </Typography>
         </div>
     );
