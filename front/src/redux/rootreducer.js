74550c86b094bdd15bd912f1eaf74688bd344183
togami <tuabobo123@gmail.com>
2020-05-22 18:38:12 +0900

    step5-5-new-stable

--- front/src/redux/rootreducer.js
+++ front/src/redux/rootreducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import calendarReducer from "./calendar/reducer";
 import addScheduleReducer from "./addSchedule/reducer"
 import schedulesReducer from "./schedules/reducer";
+import currentScheduleReducer from "./currentSchedule/reducer";
 
 const rootReducer = combineReducers({
     calendar: calendarReducer,
     addSchedule: addScheduleReducer,
-    schedules: schedulesReducer
+    schedules: schedulesReducer,
+    currentSchedule: currentScheduleReducer
 });
 
 export default rootReducer;
